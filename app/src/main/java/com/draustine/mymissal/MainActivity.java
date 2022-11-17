eb0ac3db8c701d1d421f79e545ca0e52551a7bf9
Austine Ezemelue <draustine@yahoo.com>
2022-11-17 12:26:18 +0100

    Finished

--- app/src/main/java/com/draustine/mymissal/MainActivity.java
+++ app/src/main/java/com/draustine/mymissal/MainActivity.java
@@ -1,14 +1,114 @@
 package com.draustine.mymissal;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Window;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    WebView web;
+    private String[] PERMISSIONS;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        PERMISSIONS = new String[]{
+                Manifest.permission.INTERNET,
+                Manifest.permission.SEND_SMS,
+                Manifest.permission.ACCESS_NETWORK_STATE,
+                Manifest.permission.READ_PHONE_STATE,
+                Manifest.permission.READ_PHONE_NUMBERS,
+                Manifest.permission.READ_SMS
+        };
+        getPermissions();
+
+        web = findViewById(R.id.web);
+        web.setWebViewClient(new WebViewClient());
+        web.loadUrl("http://universalis.com/Africa.Nigeria/today.htm");
+        WebSettings webSettings = web.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+
+
     }
+
+    public class mywebClient extends WebViewClient {
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view,url,favicon);
+        }
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+    @Override
+    public void onBackPressed() {
+        if (web.canGoBack()) {
+            web.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+    private void getPermissions() {
+
+        if (!hasPermissions(MainActivity.this, PERMISSIONS)) {
+            ActivityCompat.requestPermissions(MainActivity.this, PERMISSIONS, 1);
+        }
+    }
+
+    // Checks whether permissions have been granted
+    private boolean hasPermissions(Context context, String... PERMISSIONS) {
+
+        if (context != null && PERMISSIONS != null) {
+            for (String permission : PERMISSIONS) {
+                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    // Returns the responses from permission requests
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 1) {
+            int x = 0;
+            String tempStr, comment;
+            for (String s : PERMISSIONS) {
+                int m = s.length();
+                tempStr = s.substring(19, m);
+                if (grantResults[x] == PackageManager.PERMISSION_GRANTED) {
+                    comment = tempStr + " permission is granted";
+                } else {
+                    comment = tempStr + " permission is denied";
+                }
+                Toast.makeText(this, comment, Toast.LENGTH_LONG).show();
+                x++;
+            }
+        }
+
+    }
+
 }
